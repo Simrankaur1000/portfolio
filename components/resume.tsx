80c18642210bbd8d5e36692fcd6ce78b01c40d32
Simrankaur1000 <66764436+Simrankaur1000@users.noreply.github.com>
2026-09-16 21:39:56 +0530

    Update resume.tsx

--- components/resume.tsx
+++ components/resume.tsx
@@ -11,7 +11,7 @@ const highlights = [
   'Interested in VAPT, SOC and Cyber Defense',
 ]
 
-const resumePath = '/resume/CV.pdf'
+const resumePath = '/resume/resume.pdf'
 
 export function Resume() {
   return (
